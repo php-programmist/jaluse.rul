a54fb49f30178c01d173676e096281c2dd402c94
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-12-14 16:47:25 +0300

    Добавлена админка

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -254,4 +254,9 @@ class Page
 
         return $this;
     }
+    
+    public function __toString()
+    {
+        return $this->getName();
+    }
 }
--- src/Entity/Type.php
+++ src/Entity/Type.php
@@ -80,4 +80,8 @@ class Type
 
         return $this;
     }
+    public function __toString()
+    {
+        return $this->getName();
+    }
 }
