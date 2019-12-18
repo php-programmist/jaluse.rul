bb3d43ac8dbfbde4d97e31b6ea4820d3410a60e4
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-12-18 21:47:26 +0300

    Установил фреймворк для тестирования

---
+++ tests/ApplicationAvailabilityFunctionalTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+        
+        
+        $this->assertResponseIsSuccessful("Wrong url is $url");
+    }
+    
+    public function testPageIsNotFound()
+    {
+        $client = self::createClient();
+        $client->request('GET', '/abrakadabra/');
+        
+        $this->assertResponseStatusCodeSame(404);
+    }
+    
+    public function testPageIsRedirect()
+    {
+        $client = self::createClient();
+        $client->request('GET', '/admin/');
+        
+        $this->assertResponseRedirects('/login');
+    }
+    
+    
+    public function urlProvider()
+    {
+        yield ['/'];
+        yield ['/zhalyuzi/'];
+        yield ['/zhalyuzi/vertikalnye/tkanevye/avrora-persikovyj/'];
+        yield ['/api/main-page-calc/getInitData'];
+        yield ['/api/main-page-calc/getProducts'];
+        
+    }
+}
