2ad421204b0d71f46db1cfbcf0f0bb525f9c6cd3
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2020-02-13 14:03:02 +0300

    Названия товаров и мета-тэги

--- src/Controller/Admin/ImportController.php
+++ src/Controller/Admin/ImportController.php
@@ -9,10 +9,12 @@ use App\Repository\MaterialRepository;
 use App\Repository\ProductRepository;
 use App\Repository\TypeRepository;
 use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * @Route("/admin/import", name="admin_import_")
  */
@@ -38,58 +40,72 @@ class ImportController extends AbstractController
      * @var CategoryRepository
      */
     protected $category_repository;
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
     
-    public function __construct(Connection $connection,ParameterBagInterface $params,ProductRepository $product_repository,CatalogRepository $catalog_repository, CategoryRepository $category_repository)
-    {
+    public function __construct(
+        Connection $connection,
+        ParameterBagInterface $params,
+        ProductRepository $product_repository,
+        CatalogRepository $catalog_repository,
+        CategoryRepository $category_repository,
+        EntityManagerInterface $em
+    ) {
         
-        $this->connection = $connection;
-        $this->params = $params;
-        $this->product_repository = $product_repository;
-        $this->catalog_repository = $catalog_repository;
+        $this->connection          = $connection;
+        $this->params              = $params;
+        $this->product_repository  = $product_repository;
+        $this->catalog_repository  = $catalog_repository;
         $this->category_repository = $category_repository;
+        $this->em                  = $em;
     }
+    
     /**
      * @Route("/mini", name="mini")
      */
     public function mini()
     {
-        $project_dir = $this->params->get('kernel.project_dir');
-        $fh = fopen($project_dir.'/csv/mini.csv','r');
+        $project_dir   = $this->params->get('kernel.project_dir');
+        $fh            = fopen($project_dir . '/csv/mini.csv', 'r');
         $entityManager = $this->getDoctrine()->getManager();
-        $parent = $this->catalog_repository->find(2801);
-        $counter = 0;
-        while ($row = fgetcsv($fh,8000,';')){
-            [$uri,$matrix_id,$category] = $row;
-            $product = $this->product_repository->findOneBy(['uri'=>$uri]);
+        $parent        = $this->catalog_repository->find(2801);
+        $counter       = 0;
+        while ($row = fgetcsv($fh, 8000, ';')) {
+            [$uri, $matrix_id, $category] = $row;
+            $product = $this->product_repository->findOneBy(['uri' => $uri]);
             if ( ! $product) {
-                throw new \Exception('не найден товар:'.$uri);
+                throw new \Exception('не найден товар:' . $uri);
             }
-            $category = $this->category_repository->find($category);
+            $category     = $this->category_repository->find($category);
             $copy_product = clone $product;
             $copy_product->setParent($parent);
             $copy_product->setMatrixId($matrix_id);
             $copy_product->setCategory($category);
-            $copy_product->setUri(str_replace('kollekcziya-euro-vista-novinka-sezona-2014','mini',$uri));
+            $copy_product->setUri(str_replace('kollekcziya-euro-vista-novinka-sezona-2014', 'mini', $uri));
             $entityManager->persist($copy_product);
             $counter++;
         }
         $entityManager->flush();
+        
         return new Response($counter);
     }
+    
     /**
      * @Route("/uni", name="uni")
      */
     public function uni()
     {
-        $project_dir = $this->params->get('kernel.project_dir');
-        $fh = fopen($project_dir.'/csv/uni.csv','r');
+        $project_dir   = $this->params->get('kernel.project_dir');
+        $fh            = fopen($project_dir . '/csv/uni.csv', 'r');
         $entityManager = $this->getDoctrine()->getManager();
-        $counter = 0;
-        while ($row = fgetcsv($fh,8000,';')){
-            [$uri,$matrix_id,$category] = $row;
-            $product = $this->product_repository->findOneBy(['uri'=>$uri]);
+        $counter       = 0;
+        while ($row = fgetcsv($fh, 8000, ';')) {
+            [$uri, $matrix_id, $category] = $row;
+            $product = $this->product_repository->findOneBy(['uri' => $uri]);
             if ( ! $product) {
-                throw new \Exception('не найден товар:'.$uri);
+                throw new \Exception('не найден товар:' . $uri);
             }
             $category = $this->category_repository->find($category);
             $product->setMatrixId($matrix_id);
@@ -97,22 +113,24 @@ class ImportController extends AbstractController
             $counter++;
         }
         $entityManager->flush();
+        
         return new Response($counter);
     }
+    
     /**
      * @Route("/combo_uni", name="combo_uni")
      */
     public function combo_uni()
     {
-        $project_dir = $this->params->get('kernel.project_dir');
-        $fh = fopen($project_dir.'/csv/combo-uni.csv','r');
+        $project_dir   = $this->params->get('kernel.project_dir');
+        $fh            = fopen($project_dir . '/csv/combo-uni.csv', 'r');
         $entityManager = $this->getDoctrine()->getManager();
-        $counter = 0;
-        while ($row = fgetcsv($fh,8000,';')){
-            [$uri,$matrix_id,$category] = $row;
-            $product = $this->product_repository->findOneBy(['uri'=>$uri]);
+        $counter       = 0;
+        while ($row = fgetcsv($fh, 8000, ';')) {
+            [$uri, $matrix_id, $category] = $row;
+            $product = $this->product_repository->findOneBy(['uri' => $uri]);
             if ( ! $product) {
-                throw new \Exception('не найден товар:'.$uri);
+                throw new \Exception('не найден товар:' . $uri);
             }
             $category = $this->category_repository->find($category);
             $product->setMatrixId($matrix_id);
@@ -120,22 +138,24 @@ class ImportController extends AbstractController
             $counter++;
         }
         $entityManager->flush();
+        
         return new Response($counter);
     }
+    
     /**
      * @Route("/combo_small", name="combo_small")
      */
     public function combo_small()
     {
-        $project_dir = $this->params->get('kernel.project_dir');
-        $fh = fopen($project_dir.'/csv/combo-small.csv','r');
+        $project_dir   = $this->params->get('kernel.project_dir');
+        $fh            = fopen($project_dir . '/csv/combo-small.csv', 'r');
         $entityManager = $this->getDoctrine()->getManager();
-        $counter = 0;
-        while ($row = fgetcsv($fh,8000,';')){
-            [$uri,$matrix_id,$category] = $row;
-            $product = $this->product_repository->findOneBy(['uri'=>$uri]);
+        $counter       = 0;
+        while ($row = fgetcsv($fh, 8000, ';')) {
+            [$uri, $matrix_id, $category] = $row;
+            $product = $this->product_repository->findOneBy(['uri' => $uri]);
             if ( ! $product) {
-                throw new \Exception('не найден товар:'.$uri);
+                throw new \Exception('не найден товар:' . $uri);
             }
             $category = $this->category_repository->find($category);
             $product->setMatrixId($matrix_id);
@@ -143,39 +163,43 @@ class ImportController extends AbstractController
             $counter++;
         }
         $entityManager->flush();
+        
         return new Response($counter);
     }
+    
     /**
      * @Route("/standart", name="standart")
      */
     public function standart()
     {
-        $project_dir = $this->params->get('kernel.project_dir');
-        $fh = fopen($project_dir.'/csv/standart.csv','r');
+        $project_dir   = $this->params->get('kernel.project_dir');
+        $fh            = fopen($project_dir . '/csv/standart.csv', 'r');
         $entityManager = $this->getDoctrine()->getManager();
-        $counter = 0;
-        while ($row = fgetcsv($fh,8000,';')){
-            [$uri,$matrix_id,$category] = $row;
-            $product = $this->product_repository->findOneBy(['uri'=>$uri]);
+        $counter       = 0;
+        while ($row = fgetcsv($fh, 8000, ';')) {
+            [$uri, $matrix_id, $category] = $row;
+            $product = $this->product_repository->findOneBy(['uri' => $uri]);
             if ( ! $product) {
-                throw new \Exception('не найден товар:'.$uri);
+                throw new \Exception('не найден товар:' . $uri);
             }
             $category = $this->category_repository->find($category);
             $product->setMatrixId($matrix_id);
             $product->setCategory($category);
-            $product->setUri(str_replace('kollekcziya-euro-vista-novinka-sezona-2014','standartnye-rulonnye-shtory',$uri));
+            $product->setUri(str_replace('kollekcziya-euro-vista-novinka-sezona-2014', 'standartnye-rulonnye-shtory',
+                $uri));
             $counter++;
         }
         $entityManager->flush();
+        
         return new Response($counter);
     }
     
     /**
      * @Route("/sub_types", name="sub_types")
      */
-    public function sub_types(TypeRepository $type_repository,MaterialRepository $material_repository)
+    public function sub_types(TypeRepository $type_repository, MaterialRepository $material_repository)
     {
-        $type = $type_repository->find(133);
+        $type          = $type_repository->find(133);
         $sub_types_map = [
             191 => 'combo-small',
             192 => 'combo-uni',
@@ -185,7 +209,7 @@ class ImportController extends AbstractController
         ];
         foreach ($sub_types_map as $id => $matrix_folder) {
             $material = $material_repository->find($id);
-            $catalog = $this->catalog_repository->findOneBy(['matrix_folder'=>$matrix_folder]);
+            $catalog  = $this->catalog_repository->findOneBy(['matrix_folder' => $matrix_folder]);
             foreach ($catalog->getPages() as $page) {
                 if ($page instanceof Product) {
                     $page->setMaterial($material);
@@ -194,5 +218,31 @@ class ImportController extends AbstractController
             }
         }
         $this->getDoctrine()->getManager()->flush();
+    }
+    
+    /**
+     * @Route("/change_names", name="change_names")
+     */
+    public function change_names()
+    {
+        $catalog_id = 30;
+        $catalog    = $this->catalog_repository->find($catalog_id);
+        $counter = 0;
+        foreach ($catalog->getPages() as $page) {
+            if ($page instanceof Product) {
+                $old_name = $page->getName();
+                if (stripos($old_name, 'Рулонные шторы') !== false) {
+                    $new_name = str_replace('Рулонные шторы','Стандартные рулонные шторы',$page->getName());
+                }else{
+                    $new_name = 'Стандартные рулонные шторы '.$page->getName();
+                }
+                
+                $page->setName($new_name);
+                $counter++;
+            }
+            
+        }
+        $this->em->flush();
+        return new Response($counter);
     }
 }
