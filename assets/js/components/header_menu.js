a112b6a0236c5807a5c7221e617a4bbb033010b9
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2020-04-02 17:54:28 +0300

    Выпадающее меню

--- assets/js/components/header_menu.js
+++ assets/js/components/header_menu.js
@@ -4,21 +4,21 @@ $('.headermbtn').click(function (event) {
 	$('.header-n2').slideToggle(1000);
 });
 var headertop = $('.header').height();
-$('.vip-menu').css({top: (headertop-2)+'px'});
+//$('.vip-menu').css({top: (headertop-2)+'px'});
 $(window).scroll(function () {
 	if ($(window).width() > 1199) {
 		if ($(window).scrollTop() > headertop) {
 			$('.header-n1').css({display: 'none'});
 			$('.header-n2').css({background: 'rgba(0,0,0, 0.7)'});
 			$('.headern2phone').css({display: 'inline-block'});
-			$('.vip-menu').css({top: '47px'});
+			//$('.vip-menu').css({top: '47px'});
 			
 		}
 		else {
 			$('.header-n1').css({display: 'block'});
 			$('.header-n2').css({background: 'rgba(0,0,0, 1)'});
 			$('.headern2phone').css({display: 'none'});
-			$('.vip-menu').css({top: (headertop-2)+'px'});
+			//$('.vip-menu').css({top: 0});
 		}
 	}
 });
