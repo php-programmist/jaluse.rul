afafb2d94eb1f722d9178e852bd48fbc084713ae
Алексей Костин <php.programmist.ru@gmail.com>
2020-07-09 11:13:41 +0300

    HTML-sitemap

--- src/Controller/SiteMapController.php
+++ src/Controller/SiteMapController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Page;
 use App\Model\SiteMapModel;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SiteMapController extends AbstractController
@@ -11,11 +15,27 @@ class SiteMapController extends AbstractController
     /**
      * @Route("/sitemap.xml", name="sitemap",format="xml")
      */
-    public function index(SiteMapModel $model)
+    public function xml(SiteMapModel $model)
     {
         $pages = $model->getPages();
         return $this->render('sitemap/index.xml.twig', [
             'pages' => $pages,
         ]);
+    }
+    
+    /**
+     * @Route("/sitemap/", name="sitemap_html")
+     */
+    public function html(EntityManagerInterface $em, PaginatorInterface $paginator, Request $request)
+    {
+        $query = $em->createQuery("SELECT a FROM App\Entity\Page as a WHERE a.published = 1 ORDER BY a.id");
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            1000 /*limit per page*/
+        );
+        
+        // parameters to template
+        return $this->render('sitemap/index.html.twig', ['pagination' => $pagination]);
     }
 }
