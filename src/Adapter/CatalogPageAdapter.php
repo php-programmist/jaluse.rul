4c03f07a52eb8a7b659494d264d6d492b5a987bd
Алексей Костин <php.programmist.ru@gmail.com>
2021-01-30 14:19:36 +0300

    Добавил турбо-страницы для рольставней

--- src/Adapter/CatalogPageAdapter.php
+++ src/Adapter/CatalogPageAdapter.php
@@ -4,11 +4,9 @@ namespace App\Adapter;
 
 use App\Entity\Catalog;
 use App\Entity\Contracts\TurboPageInterface;
+use App\Entity\Roll;
 use App\Service\CatalogManager;
 use Doctrine\ORM\EntityManagerInterface;
-use PhpProgrammist\YandexTurboRssGeneratorBundle\Adapters\BasePageInterface;
-use PhpProgrammist\YandexTurboRssGeneratorBundle\Adapters\RssAdapterInterface;
-use PhpProgrammist\YandexTurboRssGeneratorBundle\RssItem;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -50,9 +48,18 @@ class CatalogPageAdapter extends RssPageAdapter
      */
     protected function getText(TurboPageInterface $page): string
     {
+        $catalog = [];
+        $popular = [];
+        if ($page->getUri() === 'rolstavni') {
+            $catalog = $this->entityManager->getRepository(Roll::class)->findAll();
+        } else {
+            $popular = $this->catalogManager->getPopular($page, 8);
+        }
+    
         return $this->twig->render('turbo/catalog/content.html.twig', [
-            'page' => $page,
-            'popular' => $this->catalogManager->getPopular($page,8),
+            'page'    => $page,
+            'popular' => $popular,
+            'catalog' => $catalog,
         ]);
     }
 }
