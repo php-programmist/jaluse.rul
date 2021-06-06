426488213f810a67d39e8cf0a47e9efbadd1a480
Алексей Костин <php.programmist.ru@gmail.com>
2021-06-06 09:21:34 +0300

    Заменил Doctrine\Common\Persistence\ManagerRegistry на Doctrine\Persistence\ManagerRegistry

--- src/Repository/RollRepository.php
+++ src/Repository/RollRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Roll;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Roll|null find($id, $lockMode = null, $lockVersion = null)
