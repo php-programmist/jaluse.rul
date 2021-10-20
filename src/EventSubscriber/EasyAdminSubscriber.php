4ec2df165f070469ea07c0e53bf67f85114857a4
Алексей Костин <php.programmist.ru@gmail.com>
2021-10-20 11:14:21 +0300

    Автоматическое заполнение родителей при создании сущности Страниц через админку

--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -2,38 +2,59 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\Article;
 use App\Entity\Config;
+use App\Entity\Page;
+use App\Repository\PageRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
-    /**
-     * @var AdapterInterface
-     */
-    protected $cache;
-    
-    public function __construct(AdapterInterface $cache)
-    {
-        $this->cache = $cache;
+    public function __construct(
+        private AdapterInterface $cache,
+        private PageRepository $pageRepository,
+    ) {
     }
     
     /**
      * Returns an array of event names this subscriber wants to listen to.
      * @return array The event names to listen to
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return array(
-            'easy_admin.post_update' => array('clearCache'),
+            'easy_admin.post_update'          => ['clearCache'],
+            BeforeEntityPersistedEvent::class => ['setParent'],
         );
     }
     
-    public function clearCache(GenericEvent $event)
+    /**
+     * Устанавливает родителя для сущности, если родитель не задан
+     *
+     * @param BeforeEntityPersistedEvent $event
+     */
+    public function setParent(BeforeEntityPersistedEvent $event): void
+    {
+        $entity = $event->getEntityInstance();
+        
+        if (!$entity instanceof Page || null !== $entity->getParent()) {
+            return;
+        }
+        $parentUri = match ($entity::class) {
+            Article::class => 'articles',
+            default => '/'
+        };
+        $parent    = $this->pageRepository->findOneBy(['uri' => $parentUri]);
+        $entity->setParent($parent);
+    }
+    
+    public function clearCache(GenericEvent $event): void
     {
         $entity = $event->getSubject();
-    
+        
         if (!($entity instanceof Config)) {
             return;
         }
