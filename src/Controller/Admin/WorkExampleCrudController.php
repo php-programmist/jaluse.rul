a361384c2fa4c8b8404040d1ae985d4411b88778
Алексей Костин <php.programmist.ru@gmail.com>
2021-10-24 15:21:46 +0300

    Убрал переменную общей стоимости

--- src/Controller/Admin/WorkExampleCrudController.php
+++ src/Controller/Admin/WorkExampleCrudController.php
@@ -66,7 +66,6 @@ class WorkExampleCrudController extends AbstractCrudController
                 $location,
                 $makeDays,
                 $installDays,
-                $totalPrice,
             ];
         }
     }
