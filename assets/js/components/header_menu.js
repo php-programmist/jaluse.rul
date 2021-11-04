d02232e29f6c9edd69bfa40e65f41859dff82a49
Алексей Костин <php.programmist.ru@gmail.com>
2021-11-04 14:41:21 +0300

    Исправил отображение элементов при прокрутке фиксированной шапки

--- assets/js/components/header_menu.js
+++ assets/js/components/header_menu.js
@@ -5,22 +5,31 @@ $('.headermbtn').click(function (event) {
 });
 var headertop = $('.header').height();
 //$('.vip-menu').css({top: (headertop-2)+'px'});
-$(window).scroll(function () {
+$(window).scroll(handleStickyMenu);
+$(document).ready(handleStickyMenu);
+
+function handleStickyMenu() {
 	if ($(window).width() > 1199) {
 		if ($(window).scrollTop() > headertop) {
-			$('.header-n1').css({display: 'none'});
-			$('.header-n2').css({background: 'rgba(0,0,0, 0.7)'});
-			$('.headern2phone').css({display: 'inline-block'});
-			$('.header-search').css({display: 'inline-block'});
-			//$('.vip-menu').css({top: '47px'});
-			
-		}
-		else {
-			$('.header-n1').css({display: 'block'});
-			$('.header-n2').css({background: 'rgba(0,0,0, 1)'});
-			$('.headern2phone').css({display: 'none'});
-			$('.header-search').css({display: 'none'});
-			//$('.vip-menu').css({top: 0});
+			sticky();
+		} else {
+			notSticky();
 		}
 	}
-});
+}
+
+function notSticky() {
+	$('.header-n1').css({display: 'block'});
+	$('.header-n2').css({background: 'rgba(0,0,0, 1)'});
+	$('.headern2phone').css({display: 'none'});
+	$('.header-search').css({display: 'none'});
+	//$('.vip-menu').css({top: 0});
+}
+
+function sticky() {
+	$('.header-n1').css({display: 'none'});
+	$('.header-n2').css({background: 'rgba(0,0,0, 0.7)'});
+	$('.headern2phone').css({display: 'inline-block'});
+	$('.header-search').css({display: 'inline-block'});
+	//$('.vip-menu').css({top: '47px'});
+}
