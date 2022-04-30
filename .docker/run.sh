f71f30965fcb29fd1243a214b19c5e18cf9e9ed2
Алексей Костин <php.programmist.ru@gmail.com>
2022-04-30 13:40:30 +0300

    Настроил Docker-окружение

--- .docker/run.sh
+++ .docker/run.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-log_message()
-{
-    LOGPREFIX="[$(date '+%Y-%m-%d %H:%M:%S')][rebuild]"
-    MESSAGE=$1
-    echo "$LOGPREFIX $MESSAGE"
-}
-
 ./stop.sh
-
-log_message "Starting containers..."
-docker-compose up -d --remove-orphans --build
+docker-compose up -d --remove-orphans
--- .docker/stop.sh
+++ .docker/stop.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-log_message()
-{
-    LOGPREFIX="[$(date '+%Y-%m-%d %H:%M:%S')]"
-    MESSAGE=$1
-    echo "$LOGPREFIX $MESSAGE"
-}
-
-
-log_message "Stopping containers..."
 docker-compose stop
-
-log_message "Removing containers..."
 docker-compose rm -f
