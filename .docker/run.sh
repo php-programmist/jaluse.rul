d33b847868c430dcb8c8e304aeba2422b96eec03
Алексей Костин <php.programmist.ru@gmail.com>
2022-04-30 14:11:39 +0300

    Сделал возможным запуск скриптов из корня проекта

--- .docker/run.sh
+++ .docker/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 ./stop.sh
-docker-compose up -d --remove-orphans
+docker-compose up -d --remove-orphans --file
