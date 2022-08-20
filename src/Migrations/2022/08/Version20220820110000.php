19402ec4a0cd2d489af72f5e052edab1007294fe
Алексей Костин <php.programmist.ru@gmail.com>
2022-08-20 13:50:46 +0300

    Заполняем список популярных каталогов для Горизонтальных алюминиевых жалюзи

---
+++ src/Migrations/2022/08/Version20220820110000.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220820110000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Заполняем список популярных каталогов для Горизонтальных алюминиевых жалюзи';
+    }
+    
+    public function up(Schema $schema): void
+    {
+        
+        $this->addSql('update page set popular_categories = ? where uri = ?', [
+            '{
+            "Алюминиевые жалюзи 16 мм": "zhalyuzi/gorizontalnye/alyuminievye/16mm",
+            "Алюминиевые жалюзи 25 мм": "zhalyuzi/gorizontalnye/alyuminievye/25mm",
+            "Алюминиевые жалюзи 50 мм": "zhalyuzi/gorizontalnye/alyuminievye/50mm",
+            }',
+            'zhalyuzi/gorizontalnye/alyuminievye',
+        ]);
+    }
+    
+    public function down(Schema $schema): void
+    {
+    
+    }
+}
