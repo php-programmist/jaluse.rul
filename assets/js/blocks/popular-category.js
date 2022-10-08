80749e82da1d3fcd389f54a8465f4f4b5e38aff2
Алексей Костин <php.programmist.ru@gmail.com>
2022-10-08 16:24:30 +0300

    Вывел список популярных категорий слайдером

--- assets/js/blocks/popular-category.js
+++ assets/js/blocks/popular-category.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 document.addEventListener("DOMContentLoaded", function (event) {
 	const button = document.querySelector(".js-popular-category-show-more");
 	button.addEventListener("click", () => {
@@ -7,4 +9,30 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		}
 		button.textContent = button.textContent === 'Показать все' ? 'Скрыть' : 'Показать все';
 	});
+});
+
+$('.popular-category .category-area').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 1
+			}
+		},
+		{
+			breakpoint: 567,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	
+	]
 });
