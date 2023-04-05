e03417a64ee2f0f2769f9951a5f0e803f598e73e
Алексей Костин <php.programmist.ru@gmail.com>
2023-04-06 00:18:04 +0300

    Фильтры - правки верстки и скрипта открытия

--- assets/js/blocks/link-selector.js
+++ assets/js/blocks/link-selector.js
@@ -1,21 +1,10 @@
-import $ from 'jquery';
+const popups = [...document.getElementsByClassName('link_selector')];
 
-$('.link_selector').click(function () {
-	const closeHandler = (event) => {
-		if (!event.target.closest('.link_selector')) {
-			$(this).removeClass('open');
-		}
-	}
+window.addEventListener('click', ({target}) => {
+	const popup = target.closest('.link_selector');
+	const clickedOnClosedPopup = popup && !popup.classList.contains('open');
 	
-	if ($(this).hasClass('open')) {
-		$(this).removeClass('open');
-		document.removeEventListener('click', closeHandler, false);
-	} else {
-		$(this).addClass('open');
-		document.addEventListener(
-			'click',
-			closeHandler,
-			false
-		)
-	}
-})
+	popups.forEach(p => p.classList.remove('open'));
+	
+	if (clickedOnClosedPopup) popup.classList.add('open');
+});
