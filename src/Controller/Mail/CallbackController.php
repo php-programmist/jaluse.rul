40798c84c6ea342b2e1d5c65a771a035089fbcd1
Алексей Костин <php.programmist.ru@gmail.com>
2024-04-08 12:16:43 +0300

    Отключил отправку заявок на почту

--- src/Controller/Mail/CallbackController.php
+++ src/Controller/Mail/CallbackController.php
@@ -4,11 +4,8 @@ namespace App\Controller\Mail;
 
 use App\Request\CallbackFormRequest;
 use App\Response\MailJsonResponse;
-use App\Service\ConfigService;
 use App\Service\TelegramApiManager;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -18,46 +15,21 @@ use Twig\Environment;
 class CallbackController extends AbstractController
 {
     /**
-     * @var MailerInterface
-     */
-    protected $mailer;
-    /**
      * @var MailJsonResponse
      */
     protected $response;
     /**
-     * @var TemplatedEmail
-     */
-    protected $email;
-    /**
      * @var Environment
      */
     protected $twig;
-    private $recipients;
-    private $headers;
     
     public function __construct(
-        MailerInterface $mailer,
-        ConfigService $config,
         MailJsonResponse $response,
         Environment $twig,
         private TelegramApiManager $telegramApiManager,
     ) {
-        $this->mailer     = $mailer;
-        $this->recipients = $config->get('mail.recipients', '');
-        $from             = $config->get('mail.from', '');
         $this->response   = $response;
-        $this->email      = (new TemplatedEmail())->from($from);
-        if (strpos($this->recipients, ',') !== false) {
-            $recipients = explode(',', $this->recipients);
-            $recipients = array_map('trim', $recipients);
-            $this->email->to(...$recipients);
-        } else {
-            $this->email->to($this->recipients);
-        }
         $this->twig    = $twig;
-        $this->headers = 'MIME-Version: 1.0' . "\r\n";
-        $this->headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
     }
     
     /**
@@ -65,21 +37,9 @@ class CallbackController extends AbstractController
      */
     public function consultation(CallbackFormRequest $request)
     {
-        // $body = $this->twig->render('mail/callback/consultation.html.twig',(array)$request);
-        // if (mail($this->recipients,"Заявка на консультацию",$body,$this->headers)) {
-        //     return $this->response->success("Спасибо, отправлено!");
-        // }
-        //
-        // return $this->response->fail(error_get_last());
         if (!$this->isCsrfTokenValid('consultation', $request->token)) {
             return $this->response->fail(['Обновите страницу и повторите отправку']);
         }
-        $this->email
-            ->subject("Заявка на консультацию")
-            ->htmlTemplate('mail/callback/consultation.html.twig')
-            ->context((array)$request);
-    
-        $this->mailer->send($this->email);
     
         $this->telegramApiManager->sendAdminMessage('telegram/callback/consultation.html.twig', (array)$request);
     
@@ -91,18 +51,9 @@ class CallbackController extends AbstractController
      */
     public function order(CallbackFormRequest $request)
     {
-        // $body = $this->twig->render('mail/callback/order.html.twig',(array)$request);
-        // mail($this->recipients,"Новый заказ",$body,$this->headers);
-        // return $this->response->success("Спасибо, отправлено!");
         if (!$this->isCsrfTokenValid('consultation', $request->token)) {
             return $this->response->fail(['Обновите страницу и повторите отправку']);
         }
-        $this->email
-            ->subject("Новый заказ")
-            ->htmlTemplate('mail/callback/order.html.twig')
-            ->context((array)$request);
-    
-        $this->mailer->send($this->email);
     
         $this->telegramApiManager->sendAdminMessage('telegram/callback/order.html.twig', (array)$request);
     
